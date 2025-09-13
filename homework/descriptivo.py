5857f4bad6dab9f92e93ef0bd399af53cc713f1b
Manuela Herrera <maherreraoc@unal.edu.co>
2025-09-13 08:38:07 -0500

    Agrega archivos descriptivo.ipynb y descriptivo.py

---
+++ homework/descriptivo.py
@@ -0,0 +1,17 @@
+import pandas as pd  #  type: ignore
+
+df = pd.read_csv("../files/input/house_data.csv")
+
+features = df[
+    [
+        "bedrooms",
+        "bathrooms",
+        "sqft_living",
+        "sqft_lot",
+        "floors",
+        "waterfront",
+        "condition",
+    ]
+]
+
+target = df[["price"]]
